6aef0145f7b788d068b6e2c34e0446659bb12088
sanjaysp2001 <spsanjay6567@gmail.com>
2021-07-21 17:26:07 +0530

    Added comments for better understanding

--- routes/users.js
+++ routes/users.js
@@ -47,16 +47,4 @@ router.post('/login',passport.authenticate('local'),(req,res)=>{
     res.json({success: true,token:token,status: 'Login Successful!'});
 });
 
-router.get('/logout',(req,res,next)=>{
-    if(req.session){
-      req.session.destroy();
-      res.clearCookie('session-id');
-      res.redirect('/')
-    }
-    else{
-      var err = new Error('You are not logged in!');
-      err.status = 403;
-      next(err);
-    }
-});
 module.exports = router;
